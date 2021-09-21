2441ab26aca49205afbd13c81d7d6d6a943a2310
Oto Brglez <otobrglez@gmail.com>
2021-09-21 14:15:36 +0200

    Adding a splash of nix.

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "bicikelj-sh";
+    buildInputs = with pkgs; [
+      curl
+      jq
+      bash
+    ];
+
+    shellHook = ''
+    '';
+}
